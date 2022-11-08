60f11de22f58d8ca541f8ca6ccdc1a0ede3c10dd
Andres Delgado <andresdelgado335@gmail.com>
2022-11-08 11:34:17 -0600

    finished variables

---
+++ src/HelloWorld.java
@@ -0,0 +1,39 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello, ");
+        System.out.print("World!");
+
+        int myFavoriteNumber = 10;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "I love Java";
+        System.out.println(myString);
+
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+        int d = 5;
+        System.out.println(++d);
+        System.out.println(d);
+
+        int a = 4;
+        a += 5;
+        System.out.println(a);
+
+        int b = 3;
+        int c = 4;
+        c *= b;
+        System.out.println(c);
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+        System.out.println(y);
+    }
+}
+
