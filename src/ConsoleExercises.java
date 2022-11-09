8c1241adafce1f45f3a6b1371cfcfb7047958027
Andres Delgado <andresdelgado335@gmail.com>
2022-11-09 11:06:27 -0600

    finished exercise

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args) {
+//        double pi = 3.14159;
+//        System.out.printf("The value of pi is approximately %s", pi);
+
+        Scanner sc = new Scanner(System.in);
+
+//        System.out.println("Input number");
+//        int userNumber = myScanner.nextInt();
+//        System.out.println("You picked: " + userNumber);
+
+//        System.out.println("Enter 3 cities");
+//        String userCity1 = sc.next();
+//        String userCity2 = sc.next();
+//        String userCity3 = sc.nextLine();
+//        System.out.println("you typed: " + "\n" + userCity1 + "\n" + userCity2 + "\n" +userCity3);
+//
+//
+//        System.out.println("Input sentence");
+//        String userSentence = sc.nextLine();
+//        System.out.println("you typed: " + userSentence);
+
+//        System.out.println("Input length");
+//        int userLength = sc.nextInt();
+//        System.out.println("Input height");
+//        int userHeight = sc.nextInt();
+//        int area = userLength * userHeight;
+//        int p = (userHeight * 2) + (userLength * 2);
+//        System.out.println("The area is: " + area);
+//        System.out.println("The perimeter is: " + p);
+
+        int age = 25;
+        System.out.println("guess my age");
+
+
+
+
+
+
+    }
+}
