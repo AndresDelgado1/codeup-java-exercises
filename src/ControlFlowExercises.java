e5aa031d2e6ce3bf5843f5b0ffdba5bfd108b50b
Andres Delgado <andresdelgado335@gmail.com>
2022-11-09 14:17:58 -0600

    finished exercise 3 and 4

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+
 //        int i = 5;
 //        while(i <= 15) {
 //            System.out.println(i);
@@ -30,22 +36,57 @@ public class ControlFlowExercises {
 //        while (i <= 1000000);
 
 
-        for ( int i = 1; i <= 100; i++) {
-//            System.out.println(i);
-            if(i % 3 == 0 && i % 5 == 0){
-                System.out.println("FizzBuzz");
-            }
+//        for ( int i = 1; i <= 100; i++) {
+////            System.out.println(i);
+//            if(i % 3 == 0 && i % 5 == 0){
+//                System.out.println("FizzBuzz");
+//            }
+//
+//            else if (i % 3 == 0) {
+//                System.out.println("Fizz");
+//            }
+//            else if (i % 5 == 0) {
+//                System.out.println("Buzz");
+//            }
+//            else{
+//                System.out.println(i);
+//            }
+//        }
 
-            else if (i % 3 == 0) {
-                System.out.println("Fizz");
+
+//        System.out.println("Input a grade");
+//        int userGrade = sc.nextInt();
+//        System.out.println("Would you like to continue?");
+//        String userAnswer = sc.next();
+//        if(userAnswer.equals("yes")) {
+//            if (userGrade >= 88) {
+//                System.out.println("A");
+//            } else if (userGrade >= 80) {
+//                System.out.println("B");
+//
+//            } else if (userGrade >= 67) {
+//                System.out.println("C");
+//            } else if (userGrade >= 60) {
+//                System.out.println("D");
+//            } else {
+//                System.out.println("F");
+//            }
+//        }else{
+//            System.out.println("Please confirm to see grade");
+//        }
+
+
+        System.out.println("What number would you like to go up to?");
+        int userNumber = sc.nextInt();
+        System.out.println("would you like to continue?");
+        String userAnswer = sc.next();
+        if( userAnswer.equals("yes")){
+            System.out.println("number | squared | cubed");
+            for (int i = 1; i <= userNumber; i++) {
+                System.out.println(i + "      | " + (i * i) + "       | " + (i * i * i));
             }
-            else if (i % 5 == 0) {
-                System.out.println("Buzz");
-            }
-            else{
-                System.out.println(i);
-            }
+        } else {
+            System.out.println("Sucks to suck");
         }
-
     }
 }
