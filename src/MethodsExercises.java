6349ba9fa184270e80347391cffaad261e978b99
Andres Delgado <andresdelgado335@gmail.com>
2022-11-14 14:11:28 -0600

    finished exercise 2 and 3

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,29 +1,71 @@
+import java.util.Scanner;
+
 public class MethodsExercises {
     public static void main(String[] args){
-        addition(5, 5 );
-        subtraction(5, 5 );
-        multiplication(5, 5 );
-        division(5, 5 );
-        modulus(10, 2);
+//        addition(5, 5 );
+//        subtraction(5, 5 );
+//        multiplication(5, 5 );
+//        division(5, 5 );
+//        modulus(10, 2);
+//        getInteger(4, 10);
+        factorial();
     }
 
     public static void addition(int x, int y){
+
         System.out.println(x + y);
     }
 
     public static void subtraction(int x, int y){
+
         System.out.println(x - y);
     }
 
     public static void multiplication(int x, int y){
+
         System.out.println(x * y);
     }
 
     public static void division(int x, int y){
+
         System.out.println(x / y);
     }
 
     public static void modulus(int x, int y){
+
         System.out.println(x % y);
     }
+
+    public static int getInteger(int min, int max){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter int");
+        int input = sc.nextInt();
+        if (input > max || input < min){
+            System.out.println("invalid");
+            return getInteger(min, max);
+        }
+        System.out.printf("You picked %s", input);
+        return input;
+    }
+
+    public static void factorial(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("pick a number from 1 to 10");
+        int input = sc.nextInt();
+        System.out.println("you picked " + input);
+        System.out.println("do you want to continue?");
+        String answer = sc.next();
+
+        if(answer.equals("yes")){
+
+            int fact = 1;
+            for (int i = 1; i <= input; i++) {
+                fact = fact * i;
+                System.out.println(fact);
+            }
+//            System.out.println("factional is" + fact);
+        }
+    }
 }
+
+
