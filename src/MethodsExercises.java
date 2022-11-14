a19356823a8ee8fe3dbd954deb0e6f5a4c08317c
Andres Delgado <andresdelgado335@gmail.com>
2022-11-14 11:44:32 -0600

    finished methods

---
+++ src/MethodsExercises.java
@@ -0,0 +1,29 @@
+public class MethodsExercises {
+    public static void main(String[] args){
+        addition(5, 5 );
+        subtraction(5, 5 );
+        multiplication(5, 5 );
+        division(5, 5 );
+        modulus(10, 2);
+    }
+
+    public static void addition(int x, int y){
+        System.out.println(x + y);
+    }
+
+    public static void subtraction(int x, int y){
+        System.out.println(x - y);
+    }
+
+    public static void multiplication(int x, int y){
+        System.out.println(x * y);
+    }
+
+    public static void division(int x, int y){
+        System.out.println(x / y);
+    }
+
+    public static void modulus(int x, int y){
+        System.out.println(x % y);
+    }
+}
