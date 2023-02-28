fc7268669221ec0117057f786396c38e646c6618
Andres Delgado <andresdelgado335@gmail.com>
2023-02-28 17:51:48 -0600

    learning about primatives data

---
+++ src/practice/HelloWorld.java
@@ -0,0 +1,17 @@
+package practice;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("hello");
+
+        int x = 5;
+        System.out.print(x);
+
+        x = 1000;
+        System.out.print(x);
+
+        System.out.println();
+
+        int v = Integer.MIN_VALUE;
+    }
+}
