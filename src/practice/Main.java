fb476adb271c63d4687e352d173a46f2d3b90960
Andres Delgado <andresdelgado335@gmail.com>
2023-03-21 18:28:45 -0500

    created a new car object

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -3,15 +3,19 @@ package practice;
 public class Main {
     public static void main(String[] args) {
         Car car = new Car();
-
-        car.setMake("Porsche");
+        car.setMake("ford");
         car.setModel("Carrera");
         car.setDoors(2);
         car.setConvertible(true);
         System.out.println("Make= " + car.getMake());
-
         car.describeCar();
 
+        Car targa = new Car();
+        targa.setMake("Porsche");
+        targa.setModel("Targa");
+        targa.setDoors(2);
+        targa.setConvertible(false);
+        targa.describeCar();
     }
 
 }
