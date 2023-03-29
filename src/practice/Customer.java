878b1ed3b1d4d8bee11d3ebc5eb91ba0999b0a22
Andres Delgado <andresdelgado335@gmail.com>
2023-03-29 11:56:14 -0500

    constructor practice

---
+++ src/practice/Customer.java
@@ -0,0 +1,38 @@
+package practice;
+
+public class Customer {
+    public static void main(String[] args) {
+        Customer customer = new Customer("Andres", 1000, "email");
+        System.out.println(customer.getEmail());
+        System.out.println(customer.getName());
+    }
+    private String name;
+    private double creditLimit;
+    private String email;
+
+
+    public Customer(String name, String email) {
+        this(name, 2000, email);
+    }
+
+    public Customer (String name, double creditLimit, String email){
+        this.name = name;
+        this.creditLimit = creditLimit;
+        this.email = email;
+    }
+
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
