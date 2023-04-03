e194dd8182fca52e7633711ccf9789821d875bc6
Andres Delgado <andresdelgado335@gmail.com>
2023-04-03 12:40:23 -0500

    practiced using POJO

---
+++ src/practice/Student.java
@@ -0,0 +1,29 @@
+package practice;
+
+public class Student {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5 ; i++){
+            Student s = new Student("1" + 1,
+                    switch (i){
+                    case 1 -> "marry";
+                    case 2 -> "Carol";
+                    default -> "Anonymous";
+                    },
+                    "05/11/1984",
+                    "java");
+            System.out.println(s);
+        }
+
+    }
+    private String id;
+    private String name;
+    private String dateOfBirth;
+    private String classList;
+
+    public Student(String id, String name, String dateOfBirth, String classList) {
+        this.id = id;
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.classList = classList;
+    }
+}
