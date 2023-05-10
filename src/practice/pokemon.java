126484f655767fedd5866cb7ac79cd5e3fab7e9f
Andres Delgado <andresdelgado335@gmail.com>
2023-05-09 22:59:59 -0500

    experimented with if statements in methods

--- src/practice/pokemon.java
+++ src/practice/pokemon.java
@@ -5,42 +5,87 @@ public class pokemon {
     private int pokemonLevel;
     private String pokemonType;
 
-    public pokemon(int pokemonLevel, String pokemonName, String pokemonType ){
+    public pokemon(int pokemonLevel, String pokemonName, String pokemonType) {
         this.pokemonLevel = pokemonLevel;
         this.pokemonName = pokemonName;
         this.pokemonType = pokemonType;
     }
 
-    public int getPokemonLevel(){
+    public int getPokemonLevel() {
         return this.pokemonLevel;
     }
-    public int setPokemonLevel(int newLevel){
+
+    public int setPokemonLevel(int newLevel) {
         return pokemonLevel = newLevel;
     }
-    public String getPokemonName(){
+
+    public String getPokemonName() {
         return this.pokemonName;
     }
-    public String setPokemonName(String newName){
-        return pokemonName = newName;
+
+    public void setPokemonName(String newName) {
+        this.pokemonName = newName;
     }
-    public String getPokemonType(){
+
+    public String getPokemonType() {
         return this.pokemonType;
     }
-    public String setPokemonType(String newType){
-        return pokemonName = newType;
-    }
-    public void attack(){
-        System.out.println(pokemonName + " attacked!");
-    }
-    public void iChooseYou(){
-        System.out.println("RAWRRRRRR!");
-        System.out.println(pokemonLevel + " " + pokemonName);
+
+    public void setPokemonType(String newType) {
+        this.pokemonType = newType;
     }
 
-
-    public static void main(String[] args) {
-        pokemon one = new pokemon(31, "bulbasaur", "grass");
-        one.iChooseYou();
-        one.attack();
+    public void attack() {
+        if (pokemonType.toLowerCase().equals("grass")) {
+            System.out.println(pokemonName + " used razor leaf!");
+        } if (pokemonType.toLowerCase().equals("fire")){
+            System.out.println(pokemonName + " used flame thrower");
+        } if (pokemonType.toLowerCase().equals("water")) {
+            System.out.println(pokemonName + " used water gun");
+        }
     }
+
+        public void iChooseYou () {
+            System.out.println("RAWRRRRRR!");
+            System.out.println("Lvl." + pokemonLevel + " " + pokemonName);
+        }
+
+
+        public static void fizzBuzz(){
+            for (int i = 0; i <= 100; i++){
+                if ( i % 3 == 0 && i % 5 == 0){
+                    System.out.println("FizzBuzz");
+                } else if (i % 3 == 0) {
+                    System.out.println("Fizz");
+                } else if (i % 5 == 0) {
+                    System.out.println("buzz");
+                }
+            }
+
+        }
+
+
+        public static void main (String[]args){
+            pokemon one = new pokemon(31, "bulbasaur", "grass");
+            one.iChooseYou();
+            one.attack();
+
+            pokemon two = new pokemon(32, "squirtle", "Water");
+            two.iChooseYou();
+            two.attack();
+
+            fizzBuzz();
+//            for (int i = 0; i <= 100; i++){
+//                if ( i % 3 == 0 && i % 5 == 0){
+//                    System.out.println("FizzBuzz");
+//                } else if (i % 3 == 0) {
+//                    System.out.println("Fizz");
+//                } else if (i % 5 == 0) {
+//                    System.out.println("buzz");
+//                } else {
+//                    System.out.println(i);
+//                }
+//            }
+
+        }
 }
