8aa1b923961c1d5cbdc20582ef89937674f863c8
Andres Delgado <andresdelgado335@gmail.com>
2023-06-01 22:26:30 -0700

    practiced using if statment in for loops

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -30,36 +30,38 @@ public class Main {
 //        Main c = new Main();
 //        c.helloWorld();
 //
-//    int[] numbers = {1, 2, 3};
-//    int sum = 0;
-//    for (int i = 0; i <= numbers.length; i++){
-//        sum += numbers[i];
-//    }
-//
-//        System.out.println(sum);
+    int[] numbers = {1, 2, 3};
+    int sum = 0;
+    for (int i = 0; i <= numbers.length; i++){
+//        if (sum % 2 == 0) {
+            sum += numbers[i];
+//        }
+    }
+
+        System.out.println(sum);
 
 
-        String[] animals = {"cats", "dogs", "fish"};
-//        System.out.println(animals.length);
-        for (int i = 0; i <= animals.length; i++) {
-//            System.out.println(animals[i]);
-        }
-
-
-
-
-
+//        String[] animals = {"cats", "dogs", "fish"};
+////        System.out.println(animals.length);
+//        for (int i = 0; i <= animals.length; i++) {
+////            System.out.println(animals[i]);
+//        }
 
 
 
-        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        for (int i = 0; i <= numbers.length; i++) {
-            if (numbers[i] % 2 == 0) {
-                System.out.println(numbers[i] + " is an even number");
-            } else {
-                System.out.println(numbers[i] + " is an odd number");
-            }
-        }
+
+
+
+
+
+//        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//        for (int i = 0; i <= numbers.length; i++) {
+//            if (numbers[i] % 2 == 0) {
+//                System.out.println(numbers[i] + " is an even number");
+//            } else {
+//                System.out.println(numbers[i] + " is an odd number");
+//            }
+//        }
 
 
 
